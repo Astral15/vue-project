bbcdf917e4c3177ec2522dc890fad7d4a901e432
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-03-07 14:52:52 +0400

    settings

--- src/views/register/MainRegister.vue
+++ src/views/register/MainRegister.vue
@@ -1,15 +1,17 @@
 <script setup>
 
-import Register from './Register.vue'
+import { RouterLink } from 'vue-router'
 
-const register = [
-        
-]
+const register = (state) => {
+    console.log("vmushaob")
+}
 </script>
 
 <template>
-    <button @click="">
+    <button @click="register">
         <img class="logo" src="/src/assets/logos/registration.svg" alt="menu" width="25" height="25" />
     </button>
+    <RouterLink :to="{ name: 'Register', params: {userId: 1}}">register</RouterLink>
+    <RouterLink :to="{ name: 'Update', params: {userId: 1}}">update</RouterLink>
     <RouterView />
 </template>
